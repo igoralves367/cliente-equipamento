c87670344d6a06b9bf8c5fb20c241ad5f33165dc
igoralves367 <igor.alves367@gmail.com>
2022-11-28 15:53:57 -0300

    Inicia Projeto

---
+++ src/main/java/br/com/suprematech/clienteequipamento/ClienteEquipamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.suprematech.clienteequipamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteEquipamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteEquipamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/suprematech/clienteequipamento/ClienteEquipamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suprematech.clienteequipamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteEquipamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
