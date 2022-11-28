4154b7423344b30fbe47f98e4bb6382ab19232dd
igoralves367 <igor.alves367@gmail.com>
2022-11-28 17:15:03 -0300

    Implementacao application Service

---
+++ src/main/java/br/com/suprematech/clienteequipamento/cliente/application/service/ClienteApplicationService.java
@@ -0,0 +1,18 @@
+package br.com.suprematech.clienteequipamento.cliente.application.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.suprematech.clienteequipamento.cliente.application.api.ClienteRequest;
+import br.com.suprematech.clienteequipamento.cliente.application.api.ClienteResponse;
+import lombok.extern.log4j.Log4j2;
+@Service
+@Log4j2
+public class ClienteApplicationService implements ClienteService {
+
+	@Override
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteApplicationService - criaCliente");
+		log.info("[finaliza] ClienteApplicationService - criaCliente");
+		return null;
+	}
+}
