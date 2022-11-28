c0098790cfa44099ce0fb8a8c2ef990618e54649
igoralves367 <igor.alves367@gmail.com>
2022-11-28 16:24:42 -0300

    Implementa API Teste

--- src/main/java/br/com/suprematech/clienteequipamento/ClienteEquipamentoApplication.java
+++ src/main/java/br/com/suprematech/clienteequipamento/ClienteEquipamentoApplication.java
@@ -2,12 +2,21 @@ package br.com.suprematech.clienteequipamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClienteEquipamentoApplication {
+	
+	@GetMapping
+	public String getHineTeste() {
+		return "Cliente Equipamento - API Teste Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteEquipamentoApplication.class, args);
 	}
-
 }
